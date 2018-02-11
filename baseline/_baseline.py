d834f8db2bdeead720ef986197c974e9b667d93d
Dan Gass <dan.gass@gmail.com>
2018-02-10 23:06:14 -0600

    inherit from str

--- baseline/_baseline.py
+++ baseline/_baseline.py
@@ -7,7 +7,7 @@ from _script import Script
 SEPARATOR = '\n' + '*' * 40 + '\n'
 
 
-class Baseline(object):
+class Baseline(str):
 
     z__unequal_baselines = set()
 
@@ -17,42 +17,12 @@ class Baseline(object):
 
     z__updated_scripts = {}
 
-    def __new__(cls, text):
-
-        frame = inspect.getouterframes(inspect.currentframe())[1]
-        path = os.path.abspath(frame[1])
-        if path.endswith('.pyc'):
-            assert 0, "should we ever get here?"
-            path = path[:-4] + '.py'
-        linenum = frame[2]
-
-        key = (path, linenum)
-
-        try:
-            instance = cls._all_instances[key]
-        except KeyError:
-            instance = super(Baseline, cls).__new__(cls)
-            instance.z__path = path
-            instance.z__linenum = linenum
-            cls._all_instances[key] = instance
-        else:
-            if text != instance.z__raw_text:
-                raise RuntimeError('varying baseline text not allowed')
-
-        return instance
-
-    def __init__(self, text):
-
-        if hasattr(self, "z__raw_text"):
-            return
-
-        self.z__raw_text = text
-
+    @staticmethod
+    def _dedent(text):
         lines = text.split('\n')
 
         if len(lines) == 1:
-            self._indent = 0
-            self._lines = lines
+            indent = 0
 
         elif lines[0].strip():
             raise RuntimeError('first line must be blank')
@@ -61,35 +31,61 @@ class Baseline(object):
             raise RuntimeError('last line must only contain indent whitespace')
 
         else:
-            self._indent = len(lines[-1])
+            indent = len(lines[-1])
 
-            if any(line[:self._indent].strip() for line in lines):
+            if any(line[:indent].strip() for line in lines):
                 raise RuntimeError('indents must equal or exceed indent in last line')
 
-            self._lines = [line[self._indent:] for line in lines][1:-1]
+            lines = [line[indent:] for line in lines][1:-1]
 
-        self._updates = set()
+        return indent, '\n'.join(lines)
+
+    def __new__(cls, text):
+
+        frame = inspect.getouterframes(inspect.currentframe())[1]
+        path = os.path.abspath(frame[1])
+        if path.endswith('.pyc'):
+            assert 0, "should we ever get here?"
+            # path = path[:-4] + '.py'
+        linenum = frame[2]
+
+        key = (path, linenum)
+
+        try:
+            instance = cls._all_instances[key]
+        except KeyError:
+            indent, text = cls._dedent(text)
+            instance = super(Baseline, cls).__new__(cls, text)
+            instance.z__raw_text = text
+            instance.z__path = path
+            instance.z__linenum = linenum
+            instance._indent = indent
+            instance._updates = set()
+            cls._all_instances[key] = instance
+        else:
+            if text != instance.z__raw_text:
+                raise RuntimeError('varying baseline text not allowed')
+
+        return instance
 
     def __eq__(self, text):
-        lines = [line.rstrip() for line in text.split('\n')]
+        # don't baseline trailing whitespace to avoid pylint complaints
+        text = '\n'.join(line.rstrip() for line in text.split('\n'))
 
-        result = lines == self._lines
+        is_equal = super(Baseline, self).__eq__(text)
 
-        if not result:
-            self._updates.add('\n'.join(lines))
+        if not is_equal:
+            self._updates.add(text)
             Baseline.z__unequal_baselines.add(self)
             if not Baseline.z__registered_atexit:
                 Baseline.z__registered_atexit = True
                 atexit.register(Baseline.z__atexit_callback)
 
-        return result
+        return is_equal
 
     def __ne__(self, text):
         # for use with assertNotEqual( ) for internal regression test purposes
         return not self.__eq__(text)
-
-    def __repr__(self):
-        return repr('\n'.join(self._lines))
 
     @property
     def z__update(self):
